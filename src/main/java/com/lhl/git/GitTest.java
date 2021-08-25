2faa7221ca170066de9ae889d61196bb131df253
lhl <lhl@wudiqq.com>
2021-08-25 10:45:51 +0800

    hot-fix commit3

--- src/main/java/com/lhl/git/GitTest.java
+++ src/main/java/com/lhl/git/GitTest.java
@@ -3,13 +3,6 @@ package com.lhl.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-
-
-
-
-
-
-
         System.out.println("hello ÀîºÆÁú");
     }
 }
